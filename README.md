34cc03529a8967dd05bc533ac331291d3043d635
Robert Lawrence <29217166+rtlawrence20@users.noreply.github.com>
2025-09-04 11:58:52 -0600

    impl. methods: [add, sub, mul, div] Changed private method [_checkIsNum] -> [_validNumbers]  and impl. logic, added testSuite.js with rudimentary testing of Obj {Calculator}

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
-# summative-lab-p1-js-calc
+# JavaScript Calculator
 This repository contains a summative lab project for JavaScript. The project is a calculator capable of basic mathematical operations using two operands. 
+
+### Lab Objectives
+>Implement functions for addition, subtraction, multiplication, and division.
+>Store each calculation’s details in an array.
+>Display the history of calculations to the user.
+
+## Features
+- Add two numbers:
+- Subtract second number from first:
+- Divide second number by first:
+- Multiply two numbers:
+- Maintain a history of calculations done:
+
+##### Tools and Resources
+- VSCode
+- WSL
+- GitHub
--- index.js
+++ index.js
@@ -8,26 +8,65 @@
  * @method mul(a,b) - multiplies a and b
  * @method div(a,b) - divides a by b for b != 0
  */
-const Calculator = {
+export const Calculator = {
     calcHistory: [],
 
-    _checkIsNum(a){
+    _validNumbers(a,b){
+        //check value of 'a' isNumber. If not, log to user that input is invalid
+        let conditionNaN = (tempVar) => 
+            typeof tempVar !== 'number' || !Number.isFinite(tempVar);
 
+        if (conditionNaN(a)) {
+            console.log(`Value of operand a:[${a}] is not valid input`);
+            return false;
+        }
+        //check value of 'b' isNumber. If not, log to user that input is invalid
+        else if (conditionNaN(b)) {
+            console.log(`Value of operand b:[${b}] is not valid input`);
+            return false;
+        }
+        else{
+            return true;
+        }
     },
 
     add(a,b) {
-
+        if (this._validNumbers(a,b)) {
+            const result = a + b;
+            this.calcHistory.push(result);
+            return result;
+        }
+        return;
     },
 
     sub(a,b) {
-
+        if (this._validNumbers(a,b)) {
+            const result = a - b;
+            this.calcHistory.push(result);
+            return result;
+        }
+        return;
     },
 
     mul(a,b) {
-
+        if (this._validNumbers(a,b)) {
+            const result = a * b;
+            this.calcHistory.push(result);
+            return result;
+        }
+        return;
     },
 
-    divide(a,b) {
-
+    div(a,b) {
+        if (this._validNumbers(a,b)) {
+            if (b === 0) {
+                console.log(`The value of operand b ${[b]} cannot be 0`)
+                return null;
+            }
+            const result = a / b;
+            this.calcHistory.push(result);
+            return result;
+        }
+        return;
     }
 };
