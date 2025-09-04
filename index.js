e3d4682427aa92d7bc425d429d049228c75f0367
Robert Lawrence <29217166+rtlawrence20@users.noreply.github.com>
2025-09-04 10:26:09 -0600

    Added content to README, Added framework for Calculator Object

---
+++ index.js
@@ -0,0 +1,33 @@
+// Create an object that holds calculator and methods that will be used
+
+/**
+ * This is a calculator capable of basic mathematical operations with two operands
+ * @property {Array} calcHistory - history of previous calculations
+ * @method add(a,b) - adds a to b
+ * @method sub(a,b) - subtracts b from a
+ * @method mul(a,b) - multiplies a and b
+ * @method div(a,b) - divides a by b for b != 0
+ */
+const Calculator = {
+    calcHistory: [],
+
+    _checkIsNum(a){
+
+    },
+
+    add(a,b) {
+
+    },
+
+    sub(a,b) {
+
+    },
+
+    mul(a,b) {
+
+    },
+
+    divide(a,b) {
+
+    }
+};
